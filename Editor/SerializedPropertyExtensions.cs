e2d707653f8f62a1f9ee4f9eeaaf3ee53fd9420e
Giovanni Zambiasi <giovanni.zambiasi@gmail.com>
2022-02-12 10:06:55 -0300

    Add SerializedPropertyExtensions class

---
+++ Editor/SerializedPropertyExtensions.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+
+namespace MiddleMast.Editor
+{
+    public static class SerializedPropertyExtensions
+    {
+        public static bool ContainsArrayElement(this SerializedProperty property, UnityEngine.Object @object)
+        {
+            if (!property.isArray)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < property.arraySize; i++)
+            {
+                SerializedProperty element = property.GetArrayElementAtIndex(i);
+
+                if (element.objectReferenceValue == @object)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
